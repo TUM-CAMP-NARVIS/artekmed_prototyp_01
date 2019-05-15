51f06cb77e8babf7b262d12137ae912dc4958ef6
Michael Wechner <michael.wechner@tum.de>
2019-05-15 16:24:15 +0200

    normal Alignment and mls stub function

--- include/artekmed/PointProcessing/Common.h
+++ include/artekmed/PointProcessing/Common.h
@@ -10,11 +10,12 @@
 
 //Utility Macros
 
-#define CHECK_BIT(field,pos) (((field)&(1<<(pos)))!=0)
+#define CHECK_BIT(field, pos) (((field)&(1<<(pos)))!=0)
 
 namespace artekmed
 {
 	class DepthImageSource;
+
 	//We get an open3d::PointCloud as input data and indices/stable iterators are hard to find there, we
 	// instead use the image coordinates to identify a single Pixel
 	struct InputSampleCoordinate
@@ -23,25 +24,32 @@ namespace artekmed
 		int y;
 		uint8_t imageIndex;
 
-		InputSampleCoordinate(const int _x, const int _y, const uint8_t _imageIndex):
+		InputSampleCoordinate(const int _x, const int _y, const uint8_t _imageIndex) :
 			x(_x),
 			y(_y),
-			imageIndex(_imageIndex){}
-		InputSampleCoordinate(const InputSampleCoordinate&)=default;
-		InputSampleCoordinate(InputSampleCoordinate &&) = default;
-		bool operator==(const InputSampleCoordinate & a)const
-		{
-			return x == a.x && y==a.y && imageIndex == a.imageIndex;
-		}
-		InputSampleCoordinate& operator=(const InputSampleCoordinate&)=default;
+			imageIndex(_imageIndex)
+		{}
 
-		struct hash{
-			size_t operator()(const InputSampleCoordinate & i) const{
-				return std::hash<int>()(i.x)+std::hash<int>()(i.y)+std::hash<uint8_t>()(i.imageIndex);
+		InputSampleCoordinate(const InputSampleCoordinate &) = default;
+
+		InputSampleCoordinate(InputSampleCoordinate &&) = default;
+
+		bool operator==(const InputSampleCoordinate &a) const
+		{
+			return x == a.x && y == a.y && imageIndex == a.imageIndex;
+		}
+
+		InputSampleCoordinate &operator=(const InputSampleCoordinate &) = default;
+
+		struct hash
+		{
+			size_t operator()(const InputSampleCoordinate &i) const
+			{
+				return std::hash<int>()(i.x) + std::hash<int>()(i.y) + std::hash<uint8_t>()(i.imageIndex);
 			}
 		};
 
-		Eigen::Vector3f getPointIn3D(const std::vector<DepthImageSource> & inputImages)const;
+		Eigen::Vector3f getPointIn3D(const std::vector<DepthImageSource> &inputImages) const;
 	};
 
 
@@ -53,21 +61,49 @@ namespace artekmed
 		const float depthScaleFactor;
 
 		DepthImageSource(const cv::Mat *img,
-		                 const Eigen::Matrix3f& intrinsicMat,
-		                 const Eigen::Matrix4f& extrinsicMat,
+		                 const Eigen::Matrix3f &intrinsicMat,
+		                 const Eigen::Matrix4f &extrinsicMat,
 		                 const float depthScaleFactor) :
 			intrinsics(intrinsicMat),
 			extrinsics(extrinsicMat),
 			depthImage(img),
-			depthScaleFactor(depthScaleFactor){}
+			depthScaleFactor(depthScaleFactor)
+		{}
 	};
 
 	///*@return true, if point can be seen in this image and is not occluded
 	bool project_point_to_pixel(
-		Eigen::Vector2i & pixel,
-		const Eigen::Vector3f& point,
-		const DepthImageSource& depth_image,
+		Eigen::Vector2i &pixel,
+		const Eigen::Vector3f &point,
+		const DepthImageSource &depth_image,
 		const float radius);
+
+	// Most normal estimations return the correct direction, but the normal can be flipped (i.e. does not point outside)
+	// By knowing that cameras are always outside the object , we can find the correct direction.
+	inline void alignNormal(
+		Eigen::Vector3f &normal,
+		const Eigen::Vector3f &myPos,
+		const Eigen::Vector3f &cameraWorldPos
+	)
+	{
+		const auto b = cameraWorldPos - myPos;
+		if (normal.dot(b) < 0)
+		{
+			normal *= -1;
+		}
+	}
+
+	template<class VectorItType>
+	void mlsNormalEstimation(
+		std::vector<Eigen::Vector3f>& inputPoints,
+		Eigen::Vector3f &test,
+		const size_t mainPointIndex,
+		const uint8_t degree = 1,
+		const Eigen::Vector3f & initialEstimate = {1,0,0},
+		const Eigen::Vector3f & centroid = {-1,0,0})
+	{
+
+	}
 }
 
 #endif //ARTEKMED_P1_COMMON_H
